653ddcde954b7be0d89fcabc193363a2dcac70cc
Stanislav Stoyanov <stanislav.stoyanov@orangedot.bg>
2018-06-04 16:56:39 +0300

    Add mainnet/ropsten configuration and truffle migration script

--- truffle-config.js
+++ truffle-config.js
@@ -1,9 +1,27 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic = "one cook possible system olympic entire lava stage dust casual client member";
+
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*"
+    },
+    mainnet: {
+      host: 'localhost',
+      port: 8546,
+      network_id: 1,
+      gas: 4500000,
+      gasPrice: 10000000000
+    },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/5BwKKArg3f1tu4Rk0LZD")
+      },
+      network_id: '3',
+      gas: 4500000,
+      gasPrice: 150000000000
     },
     coverage: {
       host: "localhost",
